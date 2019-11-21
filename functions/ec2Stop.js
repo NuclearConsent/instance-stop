752b28d8cdf421602104ba7e10f0475c47afec6b
devfailure <tbanks@US-TYLERBA-7480.us.trendnet.org>
2019-11-20 20:30:03 -0600

    moved env variables

--- functions/ec2Stop.js
+++ functions/ec2Stop.js
@@ -1,30 +1,30 @@
-//const aws = require('aws-sdk');
-//var ec2 = new aws.EC2();
-//let instances = [];
-//
-//exports.handler = (context) => {
-//    // List all instances
-//    ec2.describeInstances(function (err, data) {
-//        if (err) console.log(err, err.stack);
-//        // Loop through instances
-//        for (let a = 0; a < data.Reservations.length; a++) {
-//            // Loop through tags per instance
-//            for (let b = 0; b < data.Reservations[a].Instances[0].Tags.length; b++) {
-//                if (data.Reservations[a].Instances[0].Tags[b].Key == 'code' && data.Reservations[a].Instances[0].Tags[b].Value == 'ds2020') {
-//                    // Skip if instance has correct tag
-//                    a++;
-//                    break;
-//                }
-//            }
-//            // Push instances to stop into array
-//            instances.push(data.Reservations[a].Instances[0].InstanceId);
-//        }
-//        // Stop array of instances
-//        ec2.stopInstances({
-//            InstanceIds: instances
-//        }, function (err, data) {
-//            if (err) console.log(err, err.stack);
-//            else console.log(data);
-//        });
-//    });
-//};
+const aws = require('aws-sdk');
+var ec2 = new aws.EC2();
+let instances = [];
+
+exports.handler = (context) => {
+    // List all instances
+    ec2.describeInstances(function (err, data) {
+        if (err) console.log(err, err.stack);
+        // Loop through instances
+        for (let a = 0; a < data.Reservations.length; a++) {
+            // Loop through tags per instance
+            for (let b = 0; b < data.Reservations[a].Instances[0].Tags.length; b++) {
+                if (data.Reservations[a].Instances[0].Tags[b].Key == 'code' && data.Reservations[a].Instances[0].Tags[b].Value == 'ds2020') {
+                    // Skip if instance has correct tag
+                    a++;
+                    break;
+                }
+            }
+            // Push instances to stop into array
+            instances.push(data.Reservations[a].Instances[0].InstanceId);
+        }
+        // Stop array of instances
+        ec2.stopInstances({
+            InstanceIds: instances
+        }, function (err, data) {
+            if (err) console.log(err, err.stack);
+            else console.log(data);
+        });
+    });
+};
